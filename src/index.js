602d4ccfad8bd1bbc168a59f5369c99aed4f1c55
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-03-31 18:35:15 +0530

    Util code for ApiError, ApiResponse and asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({path: "../.env"});
 
-
-connectDB();
-
+connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Err: ", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Listening at: ", process.env.PORT);
+    });
+})
+.catch((err) => {
+    console.log("MONGO connection failed: ", err);
+});
 
 
 /*
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resovle(requestHandler(req, res, next))
+        .catch(err => next(err));
+    }
+};
+
+export {asyncHandler};
+
+/*
+// Using try..catch
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    }
+    catch(error) {
+        res.status(err.code || 500).json(
+            {
+                success:false,
+                message:err.message
+            }
+        )
+    }
+}
+*/

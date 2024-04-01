2b85f39d91a56304cfe0bb23f88e7df39dd8cb17
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-04-01 14:58:33 +0530

    Add user and video Models

---
+++ src/models/video.models.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import mongooseAggregatePagination from 'mongoose-aggregate-paginate-v2';
+
+const videoSchema = new mongoose.Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number, //Duration from cloudinary
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished:{
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+videoSchema.plugin(mongooseAggregatePagination);
+
+export const Video = mongoose.model("Video", videoSchema);
